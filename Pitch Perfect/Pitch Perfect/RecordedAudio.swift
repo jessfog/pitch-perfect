619e64fadc03393e1280332edbd483a47218898d
jessfog <jessfog@gmail.com>
2015-04-23 20:02:38 -0700

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,18 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Jesse Martinez on 3/10/15.
+//  Copyright (c) 2015 Jesse Martinez. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+    
+    init() {
+        
+    }
+}
